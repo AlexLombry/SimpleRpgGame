e612667d005192a01d41a4025fd92a89b634dfa9
Alex Lombry <alex.lombry@gmail.com>
2015-10-08 21:37:07 +0200

    Starting simple design, not yet finished

--- SimpleRpgGame/ViewController.swift
+++ SimpleRpgGame/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var printLbl: UILabel!
+    @IBOutlet weak var playerHpLbl: UILabel!
+    @IBOutlet weak var enemyHpLbl: UILabel!
+    
+    @IBOutlet weak var enemyImg: UIImageView!
+
+    @IBOutlet weak var chestBtn: UIButton!
+    
+    var player: Player!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let player = Player.init(name: "Sammy", hp: 200, attack: 20, defense: 10)
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func chestPressed(sender: AnyObject) {
+        
+    }
+    
+    
+    
+    
 
 }
 
